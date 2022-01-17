9c8dc85d962e64227f100f219245c7e385c2cea8
Brian Go <brian.go.dev@gmail.com>
2022-01-16 22:04:12 -0600

    pop method of linked list

--- LinkedLists/LinkedLists.js
+++ LinkedLists/LinkedLists.js
@@ -34,12 +34,51 @@ class SinglyLinkedList {
 
     return this;
   }
+
+  // Pop method
+  // 1. If there are no nodes in the list, return undefined
+  // 2. Loop through the list until you reach the tail
+  // 3. Set the next property of the 2nd to last node to be null
+  // 4. Set the tail to be the 2nd to last node
+  // 5. Decrement the length of the list by 1
+  // 6. Return the value of the node removed
+
+  pop() {
+    if (!this.head) {
+      return undefined;
+    }
+
+    let current = this.head;
+    let newTail = current;
+
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
 }
 
-// let list = new SinglyLinkedList();
-// let i = 0;
-// while (i < 10) {
-//   list.push(i);
-//   i++;
-// }
-// console.log(list);
+// Create new list with 10 nodes
+let list = new SinglyLinkedList();
+let i = 0;
+while (i < 10) {
+  list.push(i);
+  i++;
+}
+console.log(list, "Created new list");
+
+// Pop a node from the list
+list.pop();
+console.log(list, "Popped a node");
+list.pop();
+console.log(list, "Popped a node");
+list.pop();
+console.log(list, "Popped a node");
